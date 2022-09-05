d4ffe8692c4b75f0703c7b954f6e27a58c256773
Mostakim Mullick <mustakimmullick12345@gmail.com>
2022-09-05 17:03:03 +0200

    minor changes

--- jason/agents/src/main/java/thesis/SupplyChain/accessControl.java
+++ jason/agents/src/main/java/thesis/SupplyChain/accessControl.java
@@ -1,21 +1,71 @@
 package thesis.SupplyChain;
 
+import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.RawTransactionManager;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.Transfer;
+import org.web3j.utils.Convert;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 
 public class accessControl {
+     private final static String PRIVATE_KEY = "eeb24f34479018eb70f91b3303cf16a03a3ab1123baef04de480dd3b82cbc31c";
+    private final static BigInteger GAS_LIMIT = BigInteger.valueOf(6721975L);
+    private final static BigInteger GAS_PRICE = BigInteger.valueOf(20000000000L);
+
+    private final static String RECIPIENT = "0xB9a330aA459aa8DFdB971DB36a89Ed39f2063c10";
+
     public static void main(String[] args) {
-        Web3j web3j = Web3j.build(new HttpService("http://127.0.0.1:9545/"));
-        Web3ClientVersion web3ClientVersion = null;
+        try {
+            new accessControl();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private accessControl() throws Exception {
+        Web3j web3j = Web3j.build(new HttpService("HTTP://127.0.0.1:9545"));
+        printWeb3Version(web3j);
+
+        TransactionManager transactionManager = new RawTransactionManager(
+                web3j,
+                getCredentialsFromPrivateKey()
+        );
+
+        Transfer transfer = new Transfer(web3j, transactionManager);
+
+        TransactionReceipt transactionReceipt = transfer.sendFunds(
+                RECIPIENT,
+                BigDecimal.ONE,
+                Convert.Unit.ETHER,
+                GAS_PRICE,
+                GAS_LIMIT
+        ).send();
+
+        System.out.print("Transaction = " + transactionReceipt.getTransactionHash());
+    }
+
+    private void printWeb3Version(Web3j web3j) {
+        Web3ClientVersion web3ClientVersion;
+        web3ClientVersion = null;
         try {
             web3ClientVersion = web3j.web3ClientVersion().send();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        assert web3ClientVersion != null;
         String web3ClientVersionString = web3ClientVersion.getWeb3ClientVersion();
         System.out.println("Web3 client version: " + web3ClientVersionString);
+    }
+
+    private Credentials getCredentialsFromPrivateKey() {
+        return Credentials.create(PRIVATE_KEY);
     }
 }
